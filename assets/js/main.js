90d8fd7f5481525f95d44451f5f7b32400134257
tonynguyen <tonyjsfreelancer@gmail.com>
2024-08-03 10:22:16 +0700

    localstoreage

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,17 @@ const submitElement = document.getElementById('myForm');
 // const getData = JSON.parse(window.localStorage.getItem('infomation'));
 // renderData(getData);
 
+function saveData(dataSource) {
+    window.localStorage.setItem('personal', JSON.stringify(dataSource))
+}
+
+function getDataStorage() {
+    // const data = window.localStorage.getItem('personal');
+    // const parsed = JSON.parse(data)
+    // return parsed;
+    return JSON.parse(window.localStorage.getItem('personal'));
+}
+
 // Render data
 function renderData(dataSource){
     infomationElement.innerHTML = '';
@@ -95,24 +106,25 @@ submitElement.addEventListener('submit',(event)=>{
         firstNameLength.textContent = inputFirstName.value.length < 10 ? "This min field is 10." : "";
         lastNameLength.textContent = inputLastName.value.length < 10 ? "This min field is 10." : "";
         emailAddressLength.textContent = valiEmail ? "Email must end width @gmail.com" : "";
+        return;
     }
-    else{
 
-        const infomationUser = {
-            id: Date.now(),
-            firstName: inputFirstName.value,
-            lastName: inputLastName.value,
-            emailAddress: inputEmailAddress.value,
-            address: inputAddress.value,
-            city: inputCity.value,
-            district: inputDistrict.value
-        }
-        firstNameLength.textContent = "";
-        lastNameLength.textContent = "";
-        emailAddressLength.textContent = "";
-        infomation.push(infomationUser);
-        renderData(infomation);
+    const infomationUser = {
+        id: Date.now(),
+        firstName: inputFirstName.value,
+        lastName: inputLastName.value,
+        emailAddress: inputEmailAddress.value,
+        address: inputAddress.value,
+        city: inputCity.value,
+        district: inputDistrict.value
     }
+    firstNameLength.textContent = "";
+    lastNameLength.textContent = "";
+    emailAddressLength.textContent = "";
+    infomation.push(infomationUser);
+    renderData(infomation);
+
+    saveData(infomation)
 
     const count = infomation.reduce((acc,curr) => {
         if(typeof curr === 'object'){
@@ -159,4 +171,13 @@ function edit(id){
         infomation[index] = infomationUser;
         renderData(infomation);
     }
+}
+
+window.onload = function() {
+    const dataSource = getDataStorage();
+    if(!dataSource) return;
+
+    renderData(dataSource)
+
+
 }
